e51edd78e6592962e10c261688325150528d5072
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-14 18:02:06 -0300

    <addchannel> creating the structure

--- client/src/components/ChanellListContainer.jsx
+++ client/src/components/ChanellListContainer.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+
+import { RiHospitalFill } from "react-icons/ri";
+
+const sideBar = () => {
+  <section className="channel-list__sidebar">
+    <article className="channel-list__sidebar__icon1">
+      <article className="icon1__inner">
+        <RiHospitalFill width="30" />
+      </article>
+    </article>
+    ;
+  </section>;
+};
 
 export default function ChanellListContainer() {
   return (
